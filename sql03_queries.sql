34da35e0879fea37cd2072e1e2b58d60476c6144
TejasGitH <155516517+TejasGitH@users.noreply.github.com>
2025-08-15 01:25:19 +0530

    sql03_queries.sql

--- sql03_queries.sql
+++ sql03_queries.sql
@@ -1,10 +1,12 @@
 -- Top 5 bestselling drugs by total sales
-SELECT d.DrugName, SUM(f.SalesAmount) AS TotalSales
-FROM FactSales f
-JOIN DimDrug d ON f.DrugID = d.DrugID
-GROUP BY d.DrugName
-ORDER BY TotalSales DESC
-LIMIT 5;
+select
+d.DrugID,d.DrugName,ROUND(SUM(f.SalesAmount),2) as Total_sales,rank() over (order by SUM(f.SalesAmount) DESC) as rank_num
+from dimdrug d
+join factsales f
+on d.DrugID = f.DrugID
+group by d.DrugID,d.DrugName
+order by Total_sales DESC
+limit 5;
 
 -- Sales per category per month
 SELECT dr.Category, da.Year, da.Month, SUM(f.SalesAmount) AS CategorySales
@@ -13,3 +15,4 @@ JOIN DimDrug dr ON f.DrugID = dr.DrugID
 JOIN DimDate da ON f.DateKey = da.DateKey
 GROUP BY dr.Category, da.Year, da.Month
 ORDER BY dr.Category, da.Year, da.Month;
+
